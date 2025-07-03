fc598aecf63080948c79e5914b0db678b80dcdb8
sinwonsuk <onerpg3023@gmail.com>
2025-07-03 13:11:14 +0900

    기본 백엔드 업데이트

---
+++ Assets/BackEndManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+// 뒤끝 SDK namespace 추가
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    void Start()
+    {
+        var bro = Backend.Initialize(); // 뒤끝 초기화
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
+        }
+        else
+        {
+            Debug.LogError("초기화 실패 : " + bro); // 실패일 경우 statusCode 400대 에러 발생
+        }
+
+        Test();
+    }
+
+    void Test()
+    {
+        //BackendLogin.Instance.CustomSignUp("user1", "1234"); // [추가] 뒤끝 회원가입 함수
+        BackendLogin.Instance.CustomLogin("user1", "1234"); // [추가] 뒤끝 로그인
+        //BackendLogin.Instance.UpdateNickname("원하는 이름"); // [추가] 닉네임 변겅
+
+        //BackendGameData.Instance.GameDataInsert();
+
+        BackendGameData.Instance.GameDataGet(); //[추가] 데이터 불러오기 함수
+        Debug.Log("테스트를 종료합니다.");
+    }
+}
