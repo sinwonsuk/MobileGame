4c235ba467e5c574317822d5d4b6f2774c9e7b31
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-07-12 00:55:34 +0900

    인벤토리 매니저 수정

--- Assets/김연우/Scripts/InventoryManager.cs
+++ Assets/김연우/Scripts/InventoryManager.cs
@@ -6,14 +6,13 @@ public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; private set; }
 
-    [Header("Config: ��� ��� ������")]
+    [Header("Config: 전체 재료 데이터")]
     public IngredientData[] allIngredients;
 
-    [Header("Config: ��� ��� ������")]
+    [Header("Config: 전체 런타임 재료 데이터")]
     public RunTimeIngredientData[] allRunTimeIngredients;
 
-
-    [Header("Runtime: �κ��丮 ����")]
+    [Header("Runtime: 인벤토리 슬롯")]
     public List<InventorySlot> slots = new List<InventorySlot>();
 
     public event Action OnInventoryChanged;
@@ -27,19 +26,19 @@ public class InventoryManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
 
-        // �� �ʱ� ���� ����
-
+        // 초기 슬롯 세팅
 
         for (int i = 0; i < allIngredients.Length; i++)
         {
-            //if (allRunTimeIngredients[i].ingredientQty <= 0) continue;
+            // if (allRunTimeIngredients[i].ingredientQty <= 0) continue;
             slots.Add(new InventorySlot(allIngredients[i], allRunTimeIngredients[i]));
         }
 
         OnInventoryChanged?.Invoke();
     }
+
     /// <summary>
-    /// ������ �߰�
+    /// 재료 추가
     /// </summary>
     public void AddItem(string name, int amount = 1)
     {
@@ -50,10 +49,9 @@ public class InventoryManager : MonoBehaviour
                 data.ingredientQty += amount;
                 return;
             }
-        }       
+        }
         OnInventoryChanged?.Invoke();
     }
-
 
     public int GetItemQty(string name)
     {
@@ -68,7 +66,7 @@ public class InventoryManager : MonoBehaviour
             }
         }
 
-        return 0; // �ش� �������� ���� ��� 0 ��ȯ
+        return 0; // 해당 재료가 없을 경우 0 반환
     }
 
     public string IncreaseQty(string name, int amount = 1)
@@ -96,7 +94,7 @@ public class InventoryManager : MonoBehaviour
             }
         }
         OnInventoryChanged?.Invoke();
-        return "0"; 
+        return "0";
     }
 
     public void RemoveItem(string name, int amount = 1)
@@ -105,15 +103,16 @@ public class InventoryManager : MonoBehaviour
         {
             if (data.ingredientName == name)
             {
-               // data.qty -= amount;
+                // data.qty -= amount;
                 return;
             }
         }
 
         OnInventoryChanged?.Invoke();
     }
+
     /// <summary>
-    /// ��ü ����
+    /// 전체 초기화
     /// </summary>
     public void ClearInventory()
     {
