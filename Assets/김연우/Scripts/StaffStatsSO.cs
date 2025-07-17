dc0dd87af7f498a8a500bc09d61ef5298054dd06
dusdn932 <50188283+dusdn932@users.noreply.github.com>
2025-07-17 23:41:36 +0900

    직원 so 수정

--- Assets/김연우/Scripts/StaffStatsSO.cs
+++ Assets/김연우/Scripts/StaffStatsSO.cs
@@ -1,4 +1,4 @@
-// == StaffStatsSO.cs ==
+
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "StaffStats", menuName = "Clicker/Staff Stats")]
@@ -7,25 +7,25 @@ public class StaffStatsSO : ScriptableObject
     [Header("Identity")]
     public string employeeId;     // indate key
     public string displayName;    // 직원 이름
-    public StaffType staffType;
+    public StaffType staffType;  //직원 타입 (전투, 경영, 파견)
     [Header("Runtime Level")]
-    [Tooltip("현재 레벨 (런타임에 Init 시 1로 설정됩니다)")]
-    public int level;
+    public int level;// (레벨)
 
     [Header("Base Stats")]
-    public Sprite portrait;
-    public int baseSalary;
-    public int attack_Power;      // 레벨 1 기준 공격력
-    public int attack_Speed;      // 레벨 1 기준 초당 발사 횟수
+    public Sprite portrait; //이미지
+    public int baseSalary;  //
+    public double attack_Power;      // 레벨 1 기준 공격력
+    public double attack_Speed;      // 레벨 1 기준 초당 발사 횟수
 
     [Header("Per-Level Growth")]
     [Tooltip("레벨업 시 추가되는 공격력")]
-    public int attack_PowerPerLevel = 1;
+    public double attack_PowerPerLevel = 1;
     [Tooltip("레벨업 시 추가되는 공격속도")]
-    public int attack_SpeedPerLevel = 1;
+    public double attack_SpeedPerLevel = 1;
     [Tooltip("레벨업 시 시간 단축")]
-    public float timer;
+    public float timer;//실행시간
+    public float cooltime;//쉬는시간
     [Header("Other")]
-    [TextArea] public string explain;
-    public GameObject itemPrefab;
+    [TextArea] public string explain;//설명
+    public GameObject itemPrefab;//배치할 오브젝트
 }
