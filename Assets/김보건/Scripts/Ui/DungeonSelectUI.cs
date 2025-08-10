7075c1f7aeb632d07dbc11d73e95b3638cae4324
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-08-10 20:42:16 +0900

    던전 진행중 스테이지 변경 로직 추가

--- Assets/김보건/Scripts/Ui/DungeonSelectUI.cs
+++ Assets/김보건/Scripts/Ui/DungeonSelectUI.cs
@@ -9,6 +9,16 @@ public class DungeonSelectUI : MonoBehaviour
 
     public void OnClickFloorButton(int floor)
     {
+        var controller = FindAnyObjectByType<GameController>();
+        var dungeonManager = controller.GetManager<DungeonManager>();
+
+        // 던전이 이미 진행 중이라면 (중간 이동)
+        if (selectedFloorData.isDungeonMode)
+        {
+            dungeonManager.ResetDungeonEnvironment(); // 아이템 반영 + 초기화
+        }
+
+
         dungeonInGameUI.SetActive(false);
         selectedFloorData.selectedFloor = floor;
         selectedFloorData.ResetStage();
@@ -18,15 +28,11 @@ public class DungeonSelectUI : MonoBehaviour
         // 선택한 층의 던전 활성화
         selectedFloorData.isDungeonMode = true;
         EventBus<DungeonSlideToggleEvent>.Raise(new DungeonSlideToggleEvent(true));
-        //UnityEngine.SceneManagement.SceneManager.LoadScene("BoTest");
 
         selectedFloorData.autoNextFloor = autoNextToggle.GetIsOn();
-
         EventBus<ButtonisActiveHandler>.Raise(new ButtonisActiveHandler(false));
 
-        var controller = FindAnyObjectByType<GameController>();
-        //controller.ActiveOffAll(); // 다른 매니저 끄고
-        controller.GetManager<DungeonManager>().Init();
+        dungeonManager.Init();
 
         gameObject.SetActive(false);
     }
