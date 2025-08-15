a9389085b9e4b7d3cc08e23cb4754b6e1a996d10
kang-dani <daeun4001@gmail.com>
2025-08-16 00:48:58 +0900

    오류 해결

--- Assets/김보건/Scripts/Ui/ToggleDungeonCanvas.cs
+++ Assets/김보건/Scripts/Ui/ToggleDungeonCanvas.cs
@@ -6,27 +6,27 @@ public class ToggleDungeonCanvas : MonoBehaviour
     {
         EventBus<ToggleDungeonUIEvent>.OnEvent += OnToggle;
         // 다른 UI가 켜지면 닫히도록(원하면 유지)
-        EventBus<ToggleInventoryEvent>.OnEvent += _ => CloseIfOpen();
-        EventBus<ToggleHunterShopEvent>.OnEvent += _ => CloseIfOpen();
-        EventBus<ToggleRestaurantShopEvent>.OnEvent += _ => CloseIfOpen();
+        //EventBus<ToggleInventoryEvent>.OnEvent += _ => CloseIfOpen();
+        //EventBus<ToggleHunterShopEvent>.OnEvent += _ => CloseIfOpen();
+        //EventBus<ToggleRestaurantShopEvent>.OnEvent += _ => CloseIfOpen();
     }
     void OnDestroy()
     {
         EventBus<ToggleDungeonUIEvent>.OnEvent -= OnToggle;
-        EventBus<ToggleInventoryEvent>.OnEvent -= _ => CloseIfOpen();
-        EventBus<ToggleHunterShopEvent>.OnEvent -= _ => CloseIfOpen();
-        EventBus<ToggleRestaurantShopEvent>.OnEvent -= _ => CloseIfOpen();
+        //EventBus<ToggleInventoryEvent>.OnEvent -= _ => CloseIfOpen();
+        //EventBus<ToggleHunterShopEvent>.OnEvent -= _ => CloseIfOpen();
+        //EventBus<ToggleRestaurantShopEvent>.OnEvent -= _ => CloseIfOpen();
     }
     void Start() => gameObject.SetActive(false);
-
+    
     private void OnToggle(ToggleDungeonUIEvent _)
     {
         bool next = !gameObject.activeSelf;
         Debug.Log($"[ToggleDungeonCanvas] toggle -> {next}");
         gameObject.SetActive(next);
     }
-    private void CloseIfOpen()
-    {
-        if (gameObject.activeSelf) gameObject.SetActive(false);
-    }
+    //private void CloseIfOpen()
+    //{
+    //    if (gameObject.activeSelf) gameObject.SetActive(false);
+    //}
 }
